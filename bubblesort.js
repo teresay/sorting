62b585d9c62d642a810894588831245e5ff5e1fe
siameseguy <psyhakhom@gmail.com>
2018-04-23 15:00:55 -0400

    fixed test spec

--- bubblesort.js
+++ bubblesort.js
@@ -1,28 +1,3 @@
-// let counter = 0;
-// let compare = 0;
-
-// function bubbleSort(arr) {
-//   for (i===0;i<arr.length;i++) {
-//     for (j===0;j<arr.length;j++) {
-//       if(!inOrder(arr,j))
-//     }
-//   }
-
-//   return arr;
-// }
-
-
-// function swap(arr,currentindex) {
-// if(arr[currentindex]>arr[currentindex+1]) {
-//   arr[currentindex+1]=arr[currentindex];
-//   counter++;
-// }
-// }
-
-// function inOrder(arr) {
-//   if (arr.sort)
-// }
-
 function bubbleSort(array) {
   for (var end = array.length - 1; end > 0; end--) {
     for (var j = 0; j < end; j++) {
@@ -30,15 +5,15 @@ function bubbleSort(array) {
     }
   }
   return array
- }
+}
 
- function inOrder(array, index) {
+function inOrder(array, index) {
   if (index === array.length - 1) return true
   return array[index] < array[index + 1]
- }
+}
 
- function swap(array, index) {
+function swap(array, index) {
   var oldLeftValue = array[index]
   array[index] = array[index + 1]
   array[index + 1] = oldLeftValue
- }
+}
